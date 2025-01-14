1168cf82f3672308b0e2c12f1fb8bdbe487995fa
김진기 <151550964+Jingik@users.noreply.github.com>
2025-01-14 21:18:56 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+int path[10];
+
+
+void func(int depth, int start) {
+    if (depth == M) { 
+        for (int i = 0; i < M; i++) {
+            cout << path[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = start; i <= N; i++) {
+        path[depth] = i; 
+        func(depth + 1, i);  
+    }
+}
+
+
+int main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL);               
+
+    cin >> N >> M;
+    func(0, 1);
+
+    return 0;
+}
