c7907dc1f0d3d7e6cbd5afb7c63fc372142209ec
김진기 <151550964+Jingik@users.noreply.github.com>
2025-01-15 20:59:50 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -1,64 +1,66 @@
+// 01_15.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 #include <climits> 
 
 using namespace std;
 
+vector<long long>number;
 int N;
-vector<int> numbers; 
-int operators[4];     
-long long max_num = LLONG_MIN; 
-long long min_num = LLONG_MAX; 
+int operators[4] = {0,};
+// 0 : 덧셈, 1: 뺄셈, 2: 곱셈, 3: 나눗셈
+long long max_num = LLONG_MIN;
+long long min_num = LLONG_MAX;
 
+void recur(long long current, int depth) {
+	if (depth == N) {
+		max_num = max(current, max_num);
+		min_num = min(current, min_num);
+		return;
+	}
+	for (int i = 0; i < 4; i++) {
+		if (operators[i] > 0) {
+			operators[i]--;
+			if (i == 0) {
+				recur(current + number[depth], depth + 1);
+			}
+			else if(i == 1) {
+				recur(current - number[depth], depth + 1);
+			}
+			else if (i == 2) {
+				recur(current * number[depth], depth + 1);
+			}
+			else if (i == 3) {
+				if (current > 0) {
+					recur(current / number[depth], depth + 1);
+				}
+				else {
+					recur(-(-(current) / number[depth]), depth + 1);
+				}
+			}
+			operators[i]++;
+		}
+	}
 
-void backtrack(int idx, long long current) {
-    if (idx == N) {
-        max_num = max(max_num, current);
-        min_num = min(min_num, current);
-        return;
-    }
-
-    for (int i = 0; i < 4; i++) {
-        if (operators[i] > 0) {
-            operators[i]--;
-
-            if (i == 0) 
-                backtrack(idx + 1, current + numbers[idx]);
-            else if (i == 1)
-                backtrack(idx + 1, current - numbers[idx]);
-            else if (i == 2)
-                backtrack(idx + 1, current * numbers[idx]);
-            else if (i == 3) { 
-                if (current < 0) {
-                    backtrack(idx + 1, -((-current) / numbers[idx]));
-                }
-                else {
-                    backtrack(idx + 1, current / numbers[idx]);
-                }
-            }
-
-            operators[i]++;
-        }
-    }
 }
 
-int main() {
-    cin >> N;
-    numbers.resize(N);
 
-    for (int i = 0; i < N; i++) {
-        cin >> numbers[i];
-    }
+int main()
+{
 
-    for (int i = 0; i < 4; i++) {
-        cin >> operators[i];
-    }
+	cin >> N;
+	number.resize(N);
+	for (int i = 0; i < N; i++) {
+		cin >> number[i];
+	}
+	for (int i = 0; i < 4; i++) {
+		cin >> operators[i];
+	}
 
-    backtrack(1, numbers[0]);
-
-    cout << max_num << endl;
-    cout << min_num << endl;
-
-    return 0;
+	recur(number[0], 1);
+	cout << max_num << "\n";
+	cout << min_num << "\n";
 }
