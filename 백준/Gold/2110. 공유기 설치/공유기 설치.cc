d331e9bc48112a4ade5b766fa5d2659706200640
김진기 <151550964+Jingik@users.noreply.github.com>
2025-02-04 20:05:42 +0900

    [Gold IV] Title: 공유기 설치, Time: 108 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool canInstall(vector<int>& houses, int C, int distance) {
+    int count = 1; 
+    int lastInstalled = houses[0];  
+
+    for (int i = 1; i < houses.size(); i++) {
+        if (houses[i] - lastInstalled >= distance) {
+            count++;  
+            lastInstalled = houses[i];
+        }
+    }
+    return count >= C; 
+}
+
+int main() {
+    int N, C;
+    cin >> N >> C;
+
+    vector<int> houses(N);
+    for (int i = 0; i < N; i++) {
+        cin >> houses[i];
+    }
+
+    sort(houses.begin(), houses.end());  
+
+    int left = 1; 
+    int right = houses[N - 1] - houses[0]; 
+    int result = 0;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (canInstall(houses, C, mid)) {
+            result = mid; 
+            left = mid + 1; 
+        }
+        else {
+            right = mid - 1; 
+        }
+    }
+
+    cout << result << endl;
+    return 0;
+}
