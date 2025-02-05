776807f488ed2031437178907f124d392dfcbfc4
김진기 <151550964+Jingik@users.noreply.github.com>
2025-02-05 14:56:07 +0900

    [Gold V] Title: 수 고르기, Time: 64 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> num_list(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> num_list[i];
+    }
+
+    sort(num_list.begin(), num_list.end());
+
+    int left = 0, right = 0;
+    int result = INT_MAX;
+
+    while (right < N) {
+        int diff = num_list[right] - num_list[left];
+
+        if (diff >= M) {
+            result = min(result, diff);
+            left++; 
+        }
+        else {
+            right++; 
+        }
+
+        if (left > right) {
+            right = left;
+        }
+    }
+
+    cout << result << "\n";
+    return 0;
+}
