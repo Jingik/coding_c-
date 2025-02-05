5f41ffe67a0c6dc1f62977a0dc64b8ac233a9b7a
김진기 <151550964+Jingik@users.noreply.github.com>
2025-02-05 18:24:22 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> dp(K + 1, 0); 
+    vector<pair<int, int>> items(N); 
+
+    for (int i = 0; i < N; i++) {
+        int W, V;
+        cin >> W >> V;
+        items[i] = { W, V };
+    }
+
+    for (int i = 0; i < N; i++) {
+        int weight = items[i].first, value = items[i].second;
+
+        for (int j = K; j >= weight; j--) {
+            dp[j] = max(dp[j], dp[j - weight] + value);
+        }
+    }
+
+    cout << dp[K] << '\n'; 
+    return 0;
+}
