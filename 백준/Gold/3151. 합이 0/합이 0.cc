ee06fe028f8e3e91e12c96ee6a42a4166631c88b
김진기 <151550964+Jingik@users.noreply.github.com>
2025-02-06 15:19:27 +0900

    [Gold IV] Title: 합이 0, Time: 28 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.cc
+++ 백준/Gold/3151. 합이 0/합이 0.cc
@@ -1,26 +1,48 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
+using ll = long long;
+constexpr int N = 10'000;
+int a[2*N + 1];
+int n;
 
-int N;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-int main(){
-    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-    cin >> N;
-    vector<int> v(N);
-    for(int i = 0; i < N; i++) cin >> v[i];
-
-    sort(v.begin(),v.end());
-    long long ans = 0; 
-
-    for(int i = 0; i < v.size()-1; i++){
-        for(int j = i+1; j < v.size(); j++){
-            long long cnt = v[i]+v[j];
-            int idx1 = lower_bound(v.begin()+j+1,v.end(),-cnt)-v.begin();
-            int idx2 = upper_bound(v.begin()+j+1,v.end(),-cnt)-v.begin();
-            ans += idx2 - idx1;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        a[num + N]++;
+    }
+    
+    ll ans = 0;
+    for (int i = 0; i < 2*N + 1; i++) {
+        if (a[i] == 0) continue;
+        int n_abs = -(i - N);
+        if (n_abs > 0) {
+            for (int j = n_abs>>1; j >= 0; j--) {
+                if (n_abs % 2 == 0 && j == n_abs>>1) {
+                    ans += (ll)a[i] * (a[j + N] * (a[j + N] - 1))>>1;
+                } else {
+                    ans += (ll)a[i] * a[j + N] * a[n_abs - j + N];
+                }
+            }
+        } else if (n_abs < 0) {
+            for (int j = n_abs/2; j <= -1; j++) {
+                if ((i - N) % 2 == 0 && j == n_abs>>1) {
+                    ans += (ll)a[i] * (a[j + N] * (a[j + N] - 1))>>1;
+                } else {
+                    ans += (ll)a[i] * a[j + N] * a[n_abs - j + N];
+                }
+            }
+        } else {
+            if (a[i] >= 3) {
+                ans += ((ll)a[i] * (a[i] - 1) * (a[i] - 2)) / 6;
+            }
         }
     }
+   
     cout << ans;
 }
