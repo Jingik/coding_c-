17d5f87859224392701d917d94e7d38729f0ff4b
김진기 <151550964+Jingik@users.noreply.github.com>
2025-02-07 18:56:54 +0900

    [Gold V] Title: 퇴사 2, Time: 192 ms, Memory: 19600 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.cc
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -1,38 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
+#include <algorithm> 
+
 using namespace std;
-
-int N;
-vector<int> T, P;
-int dp[1500001]; 
-
-int solve(int day) {
-    if (day > N) return 0;  
-    if (dp[day] != -1) return dp[day];  
-
-    int maxProfit = solve(day + 1);
-
-    if (day + T[day] <= N + 1) {
-        maxProfit = max(maxProfit, P[day] + solve(day + T[day]));
-    }
-
-    return dp[day] = maxProfit;
-}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    int N;
     cin >> N;
-    T.resize(N + 1);
-    P.resize(N + 1);
-    memset(dp, -1, sizeof(dp)); 
+
+    vector<int> T(N + 1), P(N + 1), dp(N + 2, 0);
 
     for (int i = 1; i <= N; i++) {
         cin >> T[i] >> P[i];
     }
 
-    cout << solve(1) << '\n'; 
+    for (int i = 1; i <= N; i++) {
+        // 현재까지의 최대 이익 유지
+        dp[i] = max(dp[i], dp[i - 1]);
+
+        // 상담을 선택하는 경우
+        if (i + T[i] <= N + 1) {
+            dp[i + T[i]] = max(dp[i + T[i]], dp[i] + P[i]);
+        }
+    }
+
+    cout << *max_element(dp.begin(), dp.end()) << '\n';
     return 0;
 }
