b9b6b72c178561533549acf73874fead02cea9da
김진기 <151550964+Jingik@users.noreply.github.com>
2025-02-07 14:44:20 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -1,28 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <climits>
 #include <algorithm>
 
 using namespace std;
 
-int dp[1000] = { 0, };
+int N;
+vector<int> P, dp;
 
+int solve(int i) {
+    if (i == 0) return 0;
+
+    if (dp[i] != -1) return dp[i];
+
+    for (int j = 1; j <= i; j++) {
+        dp[i] = max(dp[i], solve(i - j) + P[j]);
+    }
+
+    return dp[i];
+}
 
 int main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int n;
-	cin >> n;
-	vector <int> card_list(n+2);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	for (int i = 1; i < n+1; i++) {
-		cin >> card_list[i];
-	}
+    cin >> N;
+    P.resize(N + 1);
+    dp.assign(N + 1, -1); 
+    for (int i = 1; i <= N; i++) cin >> P[i];
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= i; j++) {
-			dp[i] = max(dp[i], dp[i - j] + card_list[j]);
-		}
-	}
-	cout << dp[n];
-	return 0;
+    cout << solve(N) << '\n';
+    return 0;
 }
