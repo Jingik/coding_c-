aad8a8e7e8557968b3ed9d27c5887e93869e119e
김진기 <151550964+Jingik@users.noreply.github.com>
2025-02-18 18:04:39 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,47 +1,32 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-const int MAX = 1010;
-int N;
-int Arr[MAX];
-int DP[MAX];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-void Input() {
-    cin >> N;
-    for (int i = 1; i <= N; i++) {
-        cin >> Arr[i];
+    int n;
+    cin >> n;
+
+    vector<int> A(n);
+    vector<int> dp(n, 1);
+
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
     }
-}
 
-void Solution() {
-    int Answer = 0;
-    
-    for (int i = 1; i <= N; i++) {
-        DP[i] = 1;
-        for (int j = i - 1; j >= 1; j--) {
-            if (Arr[i] > Arr[j]) {
-                DP[i] = max(DP[i], DP[j] + 1);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (A[j] < A[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
             }
         }
-        Answer = max(Answer, DP[i]);
     }
 
-    cout << Answer << "\n";
-}
-
-void Solve() {
-    Input();
-    Solution();
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    Solve();
+    cout << *max_element(dp.begin(), dp.end()) << "\n";
 
     return 0;
 }
