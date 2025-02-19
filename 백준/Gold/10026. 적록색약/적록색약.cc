8efdc2e0d1a63ea12c8556734c96c7857a12c57f
김진기 <151550964+Jingik@users.noreply.github.com>
2025-02-19 18:02:55 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,102 +1,82 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <queue>
+
 using namespace std;
 
-int dx[] = { -1,1,0,0 };
-int dy[] = { 0,0,-1,1 };
-vector <vector<int>> Map_normal;
-vector <vector<int>> Map_diff;
-vector <vector<bool>> vis_normal;
-vector <vector<bool>> vis_diff;
+int dx[] = { -1, 1, 0, 0 };
+int dy[] = { 0, 0, -1, 1 };
 int N;
+vector<vector<int>> Map_normal, Map_diff;
+vector<vector<bool>> vis_normal, vis_diff;
 
-int Bfs(int check, pair<int, int> cur) {
-	if (check == 1) {
-		queue<pair<int, int>> que;
-		que.push(cur);
-		if (vis_normal[cur.first][cur.second] == true) {
-			return 0;
-		}
-		vis_normal[cur.first][cur.second] = true;
-		while (!que.empty()) {
-			pair<int, int> cu = que.front();
-			que.pop();
-			for (int i = 0; i < 4; i++) {
-				int cx = cu.first + dx[i];
-				int cy = cu.second + dy[i];
+// BFS 함수
+int Bfs(vector<vector<int>>& Map, vector<vector<bool>>& vis, int x, int y) {
+    if (vis[x][y]) return 0; // 이미 방문한 곳이면 BFS 수행 X
 
-				if (0 <= cx && cx < N && 0 <= cy && cy < N && vis_normal[cx][cy] == false && Map_normal[cx][cy] == Map_normal[cur.first][cur.second]) {
-					que.push({ cx, cy });
-					vis_normal[cx][cy] = true;
-				}
-			}
-		}
-	}
-	else {
-		queue<pair<int, int>> que;
-		que.push(cur);
-		if (vis_diff[cur.first][cur.second] == true) {
-			return 0;
-		}
-		vis_diff[cur.first][cur.second] = true;
-		while (!que.empty()) {
-			pair<int, int> cu = que.front();
-			que.pop();
-			for (int i = 0; i < 4; i++) {
-				int cx = cu.first + dx[i];
-				int cy = cu.second + dy[i];
+    queue<pair<int, int>> q;
+    q.push({ x, y });
+    vis[x][y] = true;
+    int color = Map[x][y]; // 현재 색상 저장
 
-				if (0 <= cx && cx < N && 0 <= cy && cy < N && vis_diff[cx][cy] == false && Map_diff[cx][cy] == Map_diff[cur.first][cur.second]) {
-					que.push({ cx, cy });
-					vis_diff[cx][cy] = true;
-				}
-			}
-		}
+    while (!q.empty()) {
+        pair<int, int> cur = q.front();
+        q.pop();
+        int cur_x = cur.first, cur_y = cur.second;
 
-	}
-	return 1;
+        for (int i = 0; i < 4; i++) {
+            int nx = cur_x + dx[i];
+            int ny = cur_y + dy[i];
+
+            if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+                if (!vis[nx][ny] && Map[nx][ny] == color) {
+                    vis[nx][ny] = true;
+                    q.push({ nx, ny });
+                }
+            }
+        }
+    }
+    return 1; // 새로운 구역 탐색 완료
 }
 
-
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> N;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	Map_normal.resize(N, vector<int>(N, 0));
-	Map_diff.resize(N, vector<int>(N, 0));
-	vis_normal.resize(N, vector<bool>(N, false));
-	vis_diff.resize(N, vector<bool>(N, false));
+    cin >> N;
+    Map_normal.resize(N, vector<int>(N));
+    Map_diff.resize(N, vector<int>(N));
+    vis_normal.resize(N, vector<bool>(N, false));
+    vis_diff.resize(N, vector<bool>(N, false));
 
-	for (int i = 0; i < N; i++) {
-		string str;
-		cin >> str;
-		for (int j = 0; j < str.size(); j++) {
-			if (str[j] == 'R') {
-				Map_diff[i][j] = 1;
-				Map_normal[i][j] = 1;
-			}
-			else if (str[j] == 'G') {
-				Map_diff[i][j] = 1;
-				Map_normal[i][j] = 2;
-			}
-			else {
-				Map_diff[i][j] = 2;
-				Map_normal[i][j] = 3;
-			}
-		}
-	}
-	int nor_res = 0;
-	int dif_res = 0;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			nor_res += Bfs(1, {i,j});
-			dif_res += Bfs(2, {i,j});
-		}
-	}
+    for (int i = 0; i < N; i++) {
+        string str;
+        cin >> str;
+        for (int j = 0; j < N; j++) {
+            if (str[j] == 'R') {
+                Map_normal[i][j] = 1;
+                Map_diff[i][j] = 1;  
+            }
+            else if (str[j] == 'G') {
+                Map_normal[i][j] = 2; 
+                Map_diff[i][j] = 1;   
+            }
+            else {
+                Map_normal[i][j] = 3;  
+                Map_diff[i][j] = 3;
+            }
+        }
+    }
 
-	cout << nor_res << " " << dif_res << '\n';
-	return 0;
+    int nor_res = 0, dif_res = 0;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            nor_res += Bfs(Map_normal, vis_normal, i, j);
+            dif_res += Bfs(Map_diff, vis_diff, i, j);
+        }
+    }
+
+    cout << nor_res << " " << dif_res << '\n';
+    return 0;
 }
