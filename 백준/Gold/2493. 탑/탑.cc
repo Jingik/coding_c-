5bd6fe3978e9433520ff3a69e748d184ed550d72
김진기 <151550964+Jingik@users.noreply.github.com>
2025-03-26 14:26:56 +0900

    [Gold V] Title: 탑, Time: 68 ms, Memory: 10324 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,37 +1,31 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    int N;
-    cin >> N;
+	int N;
+	cin >> N;
 
-    vector<int> heights(N);
-    vector<int> result(N, 0);
-
-    for (int i = 0; i < N; i++) {
-        cin >> heights[i];
-    }
-
-    vector<pair<int, int>> s; 
-
-    for (int i = 0; i < N; i++) {
-        while (!s.empty() && s.back().second <= heights[i]) {
-            s.pop_back();
-        }
-        if (!s.empty()) {
-            result[i] = s.back().first + 1; 
-        }
-        s.push_back({ i, heights[i] });
-    }
-
-    for (int i = 0; i < N; i++) {
-        cout << result[i] << ' ';
-    }
-
-    return 0;
+	int h;
+	stack<pair<int, int>> stk; 
+	string ans;
+	for (int i = 1; i <= N; i++) {
+		cin >> h;
+		if (stk.empty()) {
+			stk.push({ h, i });
+			ans.append("0");
+		}
+		else {
+			while (!stk.empty() && stk.top().first < h) stk.pop();
+			if (stk.empty()) ans.append("0");
+			else ans.append(to_string(stk.top().second));
+			stk.push({ h, i });
+		}
+		ans.append(" ");
+	}
+	cout << ans;
 }
