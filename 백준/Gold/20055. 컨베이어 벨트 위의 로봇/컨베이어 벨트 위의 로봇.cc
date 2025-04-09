83465edafbc2c18bf2c45f3484e6e0bead31e0bd
김진기 <151550964+Jingik@users.noreply.github.com>
2025-04-09 16:22:45 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
@@ -1,18 +1,53 @@
 #include <iostream>
-#include <deque>
 
 using namespace std;
 
+int N, K;
+int durability[201];
+bool robots[201];
+
+void rotate() {
+    int last_durability = durability[2 * N - 1];
+    bool last_robot = robots[2 * N - 1];
+
+    for (int i = 2 * N - 1; i > 0; --i) {
+        durability[i] = durability[i - 1];
+        robots[i] = robots[i - 1];
+    }
+    durability[0] = last_durability;
+    robots[0] = false;
+
+    robots[N - 1] = false;
+}
+
+
+int move_robot() {
+    int zero_count_in_this_round = 0;
+    for (int i = N - 2; i >= 0; --i) {
+        if (robots[i] && !robots[i + 1] && durability[i + 1] > 0) {
+            robots[i] = false;
+            robots[i + 1] = true;
+            durability[i + 1]--;
+            if (durability[i + 1] == 0) zero_count_in_this_round++;
+        }
+    }
+
+    robots[N - 1] = false;
+
+    if (!robots[0] && durability[0] > 0) {
+        robots[0] = true;
+        durability[0]--;
+        if (durability[0] == 0) zero_count_in_this_round++;
+    }
+
+    return zero_count_in_this_round;
+}
+
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(nullptr);    cout.tie(nullptr);
 
-    int N, K;
     cin >> N >> K;
-
-    deque<int> durability(2 * N);
-    deque<bool> robots(2 * N, false); 
-
     for (int i = 0; i < 2 * N; ++i) {
         cin >> durability[i];
     }
@@ -20,33 +55,10 @@ int main() {
     int step = 0;
     int zero_count = 0;
 
-    while (true) {
+    while (zero_count < K) {
         step++;
-
-        durability.push_front(durability.back());
-        durability.pop_back();
-        robots.push_front(robots.back());
-        robots.pop_back();
-        robots[N - 1] = false;
-
-        for (int i = N - 2; i >= 0; --i) {
-            if (robots[i] && !robots[i + 1] && durability[i + 1] > 0) {
-                robots[i] = false;
-                robots[i + 1] = true;
-                durability[i + 1]--;
-                if (durability[i + 1] == 0) zero_count++;
-            }
-        }
-
-        robots[N - 1] = false;
-
-        if (!robots[0] && durability[0] > 0) {
-            robots[0] = true;
-            durability[0]--;
-            if (durability[0] == 0) zero_count++;
-        }
-
-        if (zero_count >= K) break;
+        rotate();
+        zero_count += move_robot();
     }
 
     cout << step << '\n';
