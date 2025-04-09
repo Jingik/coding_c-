f150ffe1d3cf8fa7fb967515e96ee3135df18a4c
김진기 <151550964+Jingik@users.noreply.github.com>
2025-04-09 16:27:40 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
@@ -8,7 +8,6 @@ bool robots[201];
 
 void rotate() {
     int last_durability = durability[2 * N - 1];
-    bool last_robot = robots[2 * N - 1];
 
     for (int i = 2 * N - 1; i > 0; --i) {
         durability[i] = durability[i - 1];
@@ -16,19 +15,16 @@ void rotate() {
     }
     durability[0] = last_durability;
     robots[0] = false;
-
-    robots[N - 1] = false;
+    robots[N - 1] = false; 
 }
 
-
-int move_robot() {
-    int zero_count_in_this_round = 0;
+void move_robot() {
     for (int i = N - 2; i >= 0; --i) {
         if (robots[i] && !robots[i + 1] && durability[i + 1] > 0) {
             robots[i] = false;
             robots[i + 1] = true;
             durability[i + 1]--;
-            if (durability[i + 1] == 0) zero_count_in_this_round++;
+            if (durability[i + 1] == 0) K--;
         }
     }
 
@@ -37,15 +33,13 @@ int move_robot() {
     if (!robots[0] && durability[0] > 0) {
         robots[0] = true;
         durability[0]--;
-        if (durability[0] == 0) zero_count_in_this_round++;
+        if (durability[0] == 0) K--;
     }
-
-    return zero_count_in_this_round;
 }
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);    cout.tie(nullptr);
+    cin.tie(nullptr); cout.tie(nullptr);
 
     cin >> N >> K;
     for (int i = 0; i < 2 * N; ++i) {
@@ -53,12 +47,11 @@ int main() {
     }
 
     int step = 0;
-    int zero_count = 0;
 
-    while (zero_count < K) {
+    while (K > 0) {
         step++;
-        rotate();
-        zero_count += move_robot();
+        rotate();  
+        move_robot();  
     }
 
     cout << step << '\n';
