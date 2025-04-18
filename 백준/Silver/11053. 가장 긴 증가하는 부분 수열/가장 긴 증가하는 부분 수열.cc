9f7524368e6d0b8daccaf8a0577f4146eb1dc3d0
김진기 <151550964+Jingik@users.noreply.github.com>
2025-04-18 23:46:53 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,32 +1,22 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
 
-    int n;
+    int n, A[1000], dp[1000];
     cin >> n;
-
-    vector<int> A(n);
-    vector<int> dp(n, 1);
+    for (int i = 0; i < n; i++) cin >> A[i];
 
     for (int i = 0; i < n; i++) {
-        cin >> A[i];
-    }
-
-    for (int i = 0; i < n; i++) {
+        dp[i] = 1;
         for (int j = 0; j < i; j++) {
-            if (A[j] < A[i]) {
-                dp[i] = max(dp[i], dp[j] + 1);
-            }
+            if (A[j] < A[i]) dp[i] = max(dp[i], dp[j] + 1);
         }
     }
 
-    cout << *max_element(dp.begin(), dp.end()) << "\n";
-
-    return 0;
+    cout << *max_element(dp, dp + n) << '\n';
 }
