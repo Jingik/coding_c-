cc7156db0b5612ba1ffa296a22d873718b3441ae
김진기 <151550964+Jingik@users.noreply.github.com>
2025-04-22 15:41:24 +0900

    [level 3] Title: 베스트앨범, Time: 0.04 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.13 MB, 시간: 0.05 ms
+메모리: 3.68 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 22일 14:48:52
+2025년 04월 22일 15:41:21
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -5,39 +5,38 @@
 
 using namespace std;
 
-struct Song {
+struct song {
     int index;
-    int plays;
+    int ct;
 };
 
-bool compareSongs(const Song& a, const Song& b) {
-    if (a.plays != b.plays) return a.plays > b.plays;
-    return a.index < b.index;
+bool compare(const song& x, const song& y) {
+    if (x.ct != y.ct) {
+        return x.ct > y.ct;
+    }
+    return x.index < y.index;
+}
+bool compare2(const pair<string, int>& x, const pair<string, int>& y) {
+    return x.second > y.second;
 }
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
-    unordered_map<string, vector<Song>> genreMap;
-    unordered_map<string, int> totalPlays;
+    unordered_map<string, vector<song>> genremap;
+    unordered_map<string, int> playnum;
 
-    for (int i = 0; i < genres.size(); ++i) {
-        genreMap[genres[i]].push_back({ i, plays[i] });
-        totalPlays[genres[i]] += plays[i];
+    for (int i = 0; i < genres.size(); i++) {
+        playnum[genres[i]] += plays[i];
+        genremap[genres[i]].push_back({i, plays[i]});
     }
-
-    vector<pair<string, int>> genreOrder(totalPlays.begin(), totalPlays.end());
-    sort(genreOrder.begin(), genreOrder.end(),
-        [](const pair<string, int>& a, const pair<string, int>& b) {
-        return a.second > b.second;
-    });
-
+    vector<pair<string, int>> order_play(playnum.begin(), playnum.end());
+    sort(order_play.begin(), order_play.end(), compare2);
     vector<int> answer;
 
-    for (const auto& [genre, _] : genreOrder) {
-        auto& songs = genreMap[genre];
-        sort(songs.begin(), songs.end(), compareSongs);
-
-        for (int i = 0; i < songs.size() && i < 2; ++i) {
-            answer.push_back(songs[i].index);
+    for (const auto& [genre, _] : order_play) {
+        auto& com = genremap[genre];
+        sort(com.begin(), com.end(), compare);
+        for (int i = 0; i < com.size() && i < 2; ++i) {
+            answer.push_back(com[i].index);
         }
     }
 
