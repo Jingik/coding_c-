dd60d4b4ddea3246f82ae0fe082be8760f0e3eb6
김진기 <151550964+Jingik@users.noreply.github.com>
2025-05-13 21:30:05 +0900

    [Gold V] Title: 입국심사, Time: 48 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Gold/3079. 입국심사/입국심사.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+
+ll N, M;
+vector<ll> times;
+
+bool isPossible(ll mid) {
+    ll check = 0;
+    for (auto time : times) {
+        check += mid / time;
+        if (check >= M) return true;
+    }
+    return check >= M;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cin >> N >> M;
+    times.resize(N);
+    
+    for (ll i = 0; i < N; i++) {
+        cin >> times[i];
+    }
+
+    ll left = 1;
+    ll right = *max_element(times.begin(), times.end()) * M;
+    ll total = right;
+
+    while (left <= right) {
+        ll mid = (left + right) / 2;
+
+        if (isPossible(mid)) {
+            total = mid;
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    cout << total << '\n';
+
+    return 0;
+}
