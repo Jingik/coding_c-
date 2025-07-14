c18a47e95aaf40a3609d52f6cfe4e799b57beff2
김진기 <151550964+Jingik@users.noreply.github.com>
2025-07-14 21:03:31 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <vector>
+using namespace std;
+int answer = INT_MIN;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+	int start, end, n, k, sum = 0;
+	cin >> n >> k; 
+	start = 0;
+	end = k - 1;
+	
+    vector <int> v(n); 
+	for (int i = 0; i < n; i++)cin >> v[i]; 
+	for (int i = 0; i <= k - 1; i++)sum += v[i]; 
+	for (int i = 0; i <= n - k; i++) {
+		answer = max(answer, sum); 
+		if (end == n - 1)break; 
+		else {	
+			sum -= v[start++]; 
+			sum += v[++end]; 
+		}
+	}
+	cout << answer; 
+}
