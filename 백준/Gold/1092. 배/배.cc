b0dddc6b0624f9f41771766d2a79fd1e5b3c0d91
김진기 <151550964+Jingik@users.noreply.github.com>
2025-08-02 20:13:32 +0900

    [Gold V] Title: 배, Time: 140 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.cc
+++ 백준/Gold/1092. 배/배.cc
@@ -30,19 +30,27 @@ int main() {
         return 0;
     }
 
-    int time = 0;
-    while (!boxes.empty()) {
-        time++;
-        int crane_idx = 0; 
+    if (cranes[0] < boxes[0]) {
+        cout << -1 << '\n';
+        return 0;
+    }
 
-        for (int i = 0; i < cranes.size(); ++i) {
-            for (int j = 0; j < boxes.size(); ++j) {
-                if (cranes[i] >= boxes[j]) {
-                    boxes.erase(boxes.begin() + j); 
-                    goto next_crane; 
+    vector<bool> moved(m, false);
+    int time = 0;
+    int moved_count = 0;
+
+    while (moved_count < m) {
+        time++;
+        int box_idx = 0;
+        for (int i = 0; i < n; ++i) {
+            while (box_idx < m) {
+                if (!moved[box_idx] && cranes[i] >= boxes[box_idx]) {
+                    moved[box_idx] = true;
+                    moved_count++;
+                    break;
                 }
+                box_idx++;
             }
-        next_crane:;
         }
     }
 
