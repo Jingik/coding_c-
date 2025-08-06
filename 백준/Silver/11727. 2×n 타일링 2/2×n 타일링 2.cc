49a2fb2c7e3212a679d61c21c8a13a7113716de9
김진기 <151550964+Jingik@users.noreply.github.com>
2025-08-06 23:50:22 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> dp(n + 1);
+
+    dp[1] = 1;
+    if (n >= 2) {
+        dp[2] = 3;
+    }
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+    }
+
+    cout << dp[n] << endl;
+
+    return 0;
+}
