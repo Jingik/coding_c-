a5951107769ce0a50dc4b57cfda0442f6d07c0a4
김진기 <151550964+Jingik@users.noreply.github.com>
2025-08-26 00:02:00 +0900

    [Silver III] Title: 주유소, Time: 20 ms, Memory: 2800 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdio>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int dist[100000];
+int cost[100000];
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int n;
+    long long total = 0;
+    long long c_now;
+
+    cin >> n;
+
+    for (int i = 1; i < n; i++) {
+        cin >> dist[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> cost[i];
+    }
+    c_now = cost[0];
+    total = c_now * dist[1];
+
+    for (int i = 1; i < n; i++) {
+        if (c_now < cost[i]) {
+            total += c_now * dist[i + 1];
+        }
+        else {
+            c_now = cost[i];
+            total += c_now * dist[i + 1];
+        }
+    }
+    cout << total << '\n';
+
+    return 0;
+}
