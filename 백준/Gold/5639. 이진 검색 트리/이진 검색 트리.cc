092181f7190bf167185ac8f6ff195f48bb099ce6
김진기 <151550964+Jingik@users.noreply.github.com>
2025-08-30 00:05:12 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 40 ms, Memory: 2500 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int n;
+vector<int> v;
+
+void go(int st, int en){
+	if(st >= en) return;
+    
+	if(st == en-1){
+		cout << v[st] << '\n';
+		return;
+	}
+    
+	int idx = st + 1;
+    
+	while(idx < en){
+		if(v[st] < v[idx]) break;
+		idx++;
+	}
+    
+	go(st+1, idx);
+	go(idx, en);
+    
+	cout << v[st] << '\n';
+	return;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+    
+	while(cin >> n){
+		v.push_back(n);
+	}
+    
+	go(0, v.size());
+	return 0;
+}
