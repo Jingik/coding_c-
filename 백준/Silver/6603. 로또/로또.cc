1d6ab493ace960e344218058b3a7ceadcddaf5c4
김진기 <151550964+Jingik@users.noreply.github.com>
2025-09-04 21:29:48 +0900

    [Silver II] Title: 로또, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int S[13];
+int lotto[6];
+int k;
+
+void getCombi(int start, int depth) {
+	if (depth == 6) {
+		for (int i = 0; i < 6; i++) {
+			cout << lotto[i] << " ";
+		}
+		cout << endl;
+		return;
+	}
+    
+	for (int i = start; i < k; i++) {
+		lotto[depth] = S[i];
+		getCombi(i+1, depth+1);
+	}
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+	while (1) {
+		cin >> k;
+		if (k == 0) break;
+		for (int i = 0; i < k; i++) {
+			cin >> S[i];
+		}
+		getCombi(0, 0);
+		cout << endl;
+	}
+}
