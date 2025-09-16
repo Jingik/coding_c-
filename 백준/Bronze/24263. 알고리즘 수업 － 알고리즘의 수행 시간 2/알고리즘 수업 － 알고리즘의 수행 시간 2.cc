fd7d6e8fb5b16e6ca3c8f7f7e2cff86ad771ab60
김진기 <151550964+Jingik@users.noreply.github.com>
2025-09-16 23:01:53 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int n ;
+    
+    cin >> n;
+    cout << n << '\n' << 1;
+    return 0;
+}
