9cab77cea0d028fcd3ce384625b044ddd1ff11ce
김진기 <151550964+Jingik@users.noreply.github.com>
2025-09-19 23:43:49 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 80 ms, Memory: 11316 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+vector<int> graph[100001];
+int visited[100001] = { 0, };
+int result[100001];
+int cnt = 0;
+
+void dfs(int r) {
+	if (visited[r] == 1) { 
+		return;
+	}
+
+	visited[r] = 1;
+	cnt++;
+	result[r] = cnt;
+
+	for (int i = 0; i < graph[r].size(); i++) {
+		dfs(graph[r][i]); 
+	}
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+	int n, m, r;
+    cin >> n >> m >> r;
+	for (int i = 1; i <= m; i++) {
+		int a, b;
+        cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	for (int i = 1; i <= n; i++) {
+		sort(graph[i].begin(), graph[i].end());
+	}
+	dfs(r);
+	for (int i = 1; i <= n; i++) {
+		cout << result[i] << '\n';
+	}
+	
+}
