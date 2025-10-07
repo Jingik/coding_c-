01536b63f0fb8e720f633f4df275206139d628b3
김진기 <151550964+Jingik@users.noreply.github.com>
2025-10-07 20:34:01 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int t;
+    int a,b;
+    
+    cin >> t;
+    while(t--)
+    {
+        cin >> a >> b;
+        cout << "yes" << endl;
+    }
+    return 0;
+}
