e9e84386fc4d71b39a75f3c5d567a21c1e4f6f3e
김진기 <151550964+Jingik@users.noreply.github.com>
2025-10-08 15:43:50 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.91 ms, Memory: 4.87 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -1,61 +1,67 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <sstream>
 #include <unordered_map>
+#include <sstream>
+#include <algorithm>
+
 using namespace std;
 
-vector<string> split(const string& str) {
+vector<string> split(const string& words){
     vector<string> result;
-    istringstream ss(str);
-    string str_buf;
-    while (getline(ss, str_buf, ' ')) {
-        result.push_back(str_buf);
+    string buf;
+    istringstream ss(words);
+    
+    while(getline(ss, buf, ' ')){
+        result.push_back(buf);
     }
     return result;
 }
 
 int solution(vector<string> friends, vector<string> gifts) {
     int answer = 0;
-    unordered_map<string, int> gift_index;
-    unordered_map<string, unordered_map<string, int>> gift_count;
-    unordered_map<string, int> next_month;
+    
+    unordered_map<string, int> gift_score;
+    unordered_map<string, unordered_map<string, int>> gift_index;
+    unordered_map<string, int> next;
 
-    for (const auto& friend_name : friends) {
-        next_month[friend_name] = 0;
-        gift_index[friend_name] = 0;
+    for(const auto& fr : friends){
+        gift_score[fr] = 0;
+        next[fr] = 0;
     }
-
-    for (const auto& gift : gifts) {
-        vector<string> names = split(gift);
-        string giver = names[0], receiver = names[1];
-        gift_count[giver][receiver]++;
-        gift_index[giver]++;
-        gift_index[receiver]--;
+    
+    vector<string> cur;
+    for(const auto& gift : gifts){
+        cur = split(gift);
+        gift_score[cur[0]]++;
+        gift_score[cur[1]]--;
+        gift_index[cur[0]][cur[1]]++;
     }
+    
+    for(int i = 0; i < friends.size(); i++){
+        for(int j = i; j < friends.size(); j++){
+            int a_b = gift_index[friends[i]][friends[j]];
+            int b_a = gift_index[friends[j]][friends[i]];
 
-    for (int i = 0; i < friends.size(); i++) {
-        for (int j = i + 1; j < friends.size(); j++) {
-            string a = friends[i], b = friends[j];
-            int a_to_b = gift_count[a][b];
-            int b_to_a = gift_count[b][a]; 
-
-            if (a_to_b > b_to_a) {
-                next_month[a]++;
-            } else if (a_to_b < b_to_a) {
-                next_month[b]++;
-            } else {
-                if (gift_index[a] > gift_index[b]) {
-                    next_month[a]++;
-                } else if (gift_index[a] < gift_index[b]) {
-                    next_month[b]++;
+            if(a_b > b_a){
+                next[friends[i]]++;
+            }
+            else if(a_b < b_a){
+                next[friends[j]]++;
+            }
+            else{
+                if(gift_score[friends[i]] > gift_score[friends[j]]){
+                    next[friends[i]]++;
                 }
+                else if(gift_score[friends[i]] < gift_score[friends[j]]){
+                    next[friends[j]]++;
+                }
+                continue;
             }
         }
     }
-
-    for (const auto& value : next_month) {
-        answer = max(answer, value.second);
+    
+    for(int i = 0; i < friends.size(); i++){
+        answer = max(answer, next[friends[i]]);
     }
 
     return answer;
