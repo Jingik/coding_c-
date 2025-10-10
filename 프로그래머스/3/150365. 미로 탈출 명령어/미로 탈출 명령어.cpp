7efad39492a7654654ab427c85d2711c0bd26462
김진기 <151550964+Jingik@users.noreply.github.com>
2025-10-10 18:56:28 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 6.74 ms, Memory: 9.73 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
@@ -1,41 +1,48 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
 int dx[] = {1, 0, 0, -1};
 int dy[] = {0, -1, 1, 0};
 char dir[] = {'d', 'l', 'r', 'u'};
-string result = "";
-int N, M, R, C, K;
+
+int N, M, X, Y, R, C;
 bool found = false;
+string answer = "";
 
-void dfs(int x, int y, int cnt, string path){
-    if (found) return;
-
-    int manhattan = abs(x - R) + abs(y- C);
-    if(cnt == K && x == R && y == C){
-        result = path;
-        found = true;
+void dfs(int idx, int k, int x, int y, string cur){
+    if(found) return;
+    
+    if(idx == k){
+        if(x == R && y == C){
+            answer = cur;
+            found = true;
+        }
         return;
     }
-    if (cnt >= K || manhattan > K - cnt || (K - cnt - manhattan) % 2 != 0) return;
-
+    
+    int distance = abs(x - R) + abs(y - C);    
+    if(idx >= k || distance > k - idx || (k - idx - distance) % 2 != 0) return;
+    
     for(int i = 0; i < 4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
+        int nx = x + dx[i], ny = y + dy[i];
         if(nx > 0 && nx <= N && ny > 0 && ny <= M){
-            dfs(nx, ny, cnt + 1, path + dir[i]);
+            dfs(idx + 1, k, nx, ny, cur + dir[i]);            
         }
     }
-
 }
 
 string solution(int n, int m, int x, int y, int r, int c, int k) {
-    N = n; M = m; R = r; C = c; K = k;
-    int manhattan = abs(x - r) + abs(y - c);
-    if (manhattan > k || (k - manhattan) % 2 != 0) return "impossible";
+    N = n, M = m, X = x, Y = y, R = r, C = c;
     
-    dfs(x, y, 0, "");
-    return result.empty() ? "impossible" : result;
+    int distance = abs(x - r) + abs(y - c);
+    if(distance > k || (k - distance) % 2 != 0) return "impossible";
+    
+    dfs(0, k, x, y, "");
+    if (!answer.empty()){
+        return answer;        
+    }
+    else return "impossible";
 }
