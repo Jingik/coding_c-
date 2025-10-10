817b25549917ec66b0d3c33d59e870f050fae03b
김진기 <151550964+Jingik@users.noreply.github.com>
2025-10-10 18:59:46 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.04 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
@@ -1,48 +1,22 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
 using namespace std;
 
-int dx[] = {1, 0, 0, -1};
-int dy[] = {0, -1, 1, 0};
-char dir[] = {'d', 'l', 'r', 'u'};
 
-int N, M, X, Y, R, C;
-bool found = false;
-string answer = "";
-
-void dfs(int idx, int k, int x, int y, string cur){
-    if(found) return;
-    
-    if(idx == k){
-        if(x == R && y == C){
-            answer = cur;
-            found = true;
-        }
-        return;
-    }
-    
-    int distance = abs(x - R) + abs(y - C);    
-    if(idx >= k || distance > k - idx || (k - idx - distance) % 2 != 0) return;
-    
-    for(int i = 0; i < 4; i++){
-        int nx = x + dx[i], ny = y + dy[i];
-        if(nx > 0 && nx <= N && ny > 0 && ny <= M){
-            dfs(idx + 1, k, nx, ny, cur + dir[i]);            
-        }
-    }
+int distance(int x, int y, int r, int c){
+    return abs(x-r)+abs(y-c);
 }
 
-string solution(int n, int m, int x, int y, int r, int c, int k) {
-    N = n, M = m, X = x, Y = y, R = r, C = c;
-    
-    int distance = abs(x - r) + abs(y - c);
-    if(distance > k || (k - distance) % 2 != 0) return "impossible";
-    
-    dfs(0, k, x, y, "");
-    if (!answer.empty()){
-        return answer;        
+
+string solution(int n, int m, int y, int x, int c, int r, int k) {
+    string answer = "";
+    while(k > 0){
+        --k;
+        if(y < n && distance(x,y+1,r,c) <= k) {answer += "d"; y+=1;}
+        else if(x > 1 && distance(x-1,y,r,c) <= k) {answer += "l"; x-=1;}
+        else if(x < m && distance(x+1,y,r,c) <= k) {answer += "r"; x+=1;}
+        else if(y > 1 && distance(x,y-1,r,c) <= k) {answer += "u"; y-=1;}
+        else if(k == 0) return "impossible";
     }
-    else return "impossible";
+    return answer;
 }
