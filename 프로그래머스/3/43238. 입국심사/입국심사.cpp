82b73d028c85766fa3339863704c99fa1d8119a9
김진기 <151550964+Jingik@users.noreply.github.com>
2025-10-24 00:00:12 +0900

    [level 3] Title: 입국심사, Time: 43.39 ms, Memory: 6.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -8,21 +8,24 @@ long long solution(int n, vector<int> times) {
     long long left = 0;
     long long right = 1LL * (*max_element(times.begin(), times.end())) * n;
     long long answer = right;
-    
+   
     while(left <= right){
-        long long mid = (right + left) / 2;
+        long long mid = (left + right) / 2;
         long long people = 0;
+        
         for(auto time : times){
             people += mid / time;
         }
         
         if(people >= n){
-            answer = mid;
             right = mid - 1;
+            answer = mid;
         }
         else{
             left = mid + 1;
         }
     }
+    
+
     return answer;
 }
