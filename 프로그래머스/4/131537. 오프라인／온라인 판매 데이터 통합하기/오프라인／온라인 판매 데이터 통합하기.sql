f3a3babff53b94ba678a6fedc100ab4427a61fb8
김진기 <151550964+Jingik@users.noreply.github.com>
2025-10-31 19:27:38 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -5,7 +5,7 @@ WITH SAMPLE AS (
         USER_ID,
         SALES_AMOUNT
     FROM
-        ONLINE_SALE
+        ONLINE_SALE 
     WHERE
         DATE_FORMAT(SALES_DATE, '%Y-%m') = '2022-03'
     UNION
@@ -15,10 +15,11 @@ WITH SAMPLE AS (
         NULL AS USER_ID,
         SALES_AMOUNT
     FROM
-        OFFLINE_SALE
+        OFFLINE_SALE 
     WHERE
         DATE_FORMAT(SALES_DATE, '%Y-%m') = '2022-03'
 )
+
 SELECT
     *
 FROM
@@ -27,32 +28,3 @@ ORDER BY
     SALES_DATE,
     PRODUCT_ID,
     USER_ID
-
-# WITH SAMPLE AS (
-#     SELECT
-#         DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS SALES_DATE,
-#         PRODUCT_ID,
-#         USER_ID,
-#         SALES_AMOUNT
-#     FROM
-#         ONLINE_SALE 
-#     WHERE
-#         DATE_FORMAT(SALES_DATE, '%Y-%m') = '2022-03'
-#     UNION
-
-#     SELECT
-#         DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS SALES_DATE,
-#         PRODUCT_ID,
-#         NULL AS USER_ID,
-#         SALES_AMOUNT
-#     FROM
-#         OFFLINE_SALE 
-#     WHERE
-#         DATE_FORMAT(SALES_DATE, '%Y-%m') = '2022-03'
-# )
-# SELECT
-#     *
-# FROM
-#     SAMPLE
-# ORDER BY
-#     SALES_DATE, PRODUCT_ID, USER_ID
