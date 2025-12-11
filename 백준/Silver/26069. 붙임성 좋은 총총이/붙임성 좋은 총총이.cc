062caccd6f56847647bb8a7ea3d4684255ce6bf7
김진기 <151550964+Jingik@users.noreply.github.com>
2025-12-12 00:21:21 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+	int N, count = 0;
+	cin >> N;
+    
+	unordered_map<string, bool> m;
+	string str1, str2;
+
+	while (N--)
+	{
+		cin >> str1 >> str2;
+
+		if (str1 == "ChongChong")
+			m.insert({ str1, true });
+		else if (str2 == "ChongChong")
+			m.insert({ str2, true });
+		if (m[str1])
+			m[str2] = true;
+		if (m[str2])
+			m[str1] = true;
+	}
+
+	for (auto& val : m)
+	{
+		if (val.second)
+			count++;
+	}
+
+	cout << count << '\n';
+    
+    return 0;
+}
