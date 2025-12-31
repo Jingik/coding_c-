ecbf7bc72baf62bfd65294ef7afde620ffb2a1aa
김진기 <151550964+Jingik@users.noreply.github.com>
2025-12-31 10:46:45 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int X;
+    cin >> X;
+ 
+    int ans = 0;
+    while (X>0) {
+        if (X % 2 == 1) {
+            ans++;
+        }
+        X /= 2;
+    }
+ 
+    cout << ans;
+ 
+}
