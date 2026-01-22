609f038a75e9b5f6418981c48a79c7e27dda7e6f
김진기 <151550964+Jingik@users.noreply.github.com>
2026-01-22 15:01:10 +0900

    [Bronze III] Title: 전자레인지, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int num;
+	cin >> num;
+
+	if (num % 10 != 0) {
+		cout << -1 << '\n';
+		return 0;
+	}
+
+	int a = num / 300;
+	num %= 300;
+
+	int b = num / 60;
+	num %= 60;
+
+	int c = num / 10;
+
+	cout << a << ' ' << b << ' ' << c << '\n';
+    
+    return 0;
+}
