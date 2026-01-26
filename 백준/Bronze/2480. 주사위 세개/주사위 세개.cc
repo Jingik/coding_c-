b70bec0ce7c5479435ae3934cf093fa59a528f21
김진기 <151550964+Jingik@users.noreply.github.com>
2026-01-26 12:50:09 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+	int a, b, c;
+	cin >> a >> b >> c;
+    
+	if (a != b && b != c && a != c) {
+		int ans;
+		if (a > b) {
+			if (c > a) {
+				ans = c;
+			}
+			else {
+				ans = a;
+			}
+		}
+		else {
+			if (c > b) {
+				ans = c;
+			}
+			else {
+				ans = b;
+			}
+		}
+		cout << ans * 100;
+	}
+    
+	else {
+		if (a == b && a == c) {
+			cout << 10000 + a * 1000;
+		} else {
+			if (a == b || a == c) {
+				cout << 1000 + a * 100;
+			}
+			else {
+				cout << 1000 + b * 100;
+			}
+		}
+	}
+	return 0;
+}
