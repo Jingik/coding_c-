67060de6bcc1c289e63af6e25da1e13a3c39e840
김진기 <151550964+Jingik@users.noreply.github.com>
2026-02-18 00:14:38 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+	int a; 
+	int idx, max = 0; 
+	
+	for (int i = 1; i <= 9; i++) {
+		cin >> a;
+		if (max < a) {
+			max = a;
+			idx = i;
+		}
+	}
+
+	cout << max << '\n' << idx;
+	
+	return 0;
+}
