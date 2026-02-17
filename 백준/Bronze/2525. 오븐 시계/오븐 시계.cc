cae8c1109775bdfeed629ebc791021865e7d4384
김진기 <151550964+Jingik@users.noreply.github.com>
2026-02-17 21:40:55 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+	int A, B, C;
+	cin >> A >> B >> C;
+ 
+	int min = 60 * A + B; 
+	min += C;
+ 
+	int hour = (min / 60) % 24;
+	int minute = min % 60;
+ 
+	cout << hour << " " << minute;
+	return 0;
+}
